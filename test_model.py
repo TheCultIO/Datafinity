1f72a1c278c10efc33fe23a9d63f3791d32485bb
TheCultIO <139150823+TheCultIO@users.noreply.github.com>
2023-07-27 14:58:15 +0530

    Update test_model.py 1

--- test_model.py
+++ test_model.py
@@ -1,5 +1,6 @@
 import model
 import pandas as pd
 model.train_model()
-result_df = model.predict_model(pd.read_excel('example_data.xlsx'))
-result_df.head()
+result_df = model.predict_model(pd.read_csv('sample.csv'))
+print(result_df.head())
+print(result_df['predicted_pricePerSquareFoot'])
