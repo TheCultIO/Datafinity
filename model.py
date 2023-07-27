06a737623600ea4140f43b862fcfc4b4fb6c0a12
TheCultIO <139150823+TheCultIO@users.noreply.github.com>
2023-07-27 14:57:41 +0530

    Update model.py 1

--- model.py
+++ model.py
@@ -24,12 +24,52 @@ def segregate_columns(df):
     return categorical_cols, numerical_cols
 
 
+def remove_outliers_iqr(df, columns, threshold=1.5):
+    # Calculate the first quartile (Q1) and third quartile (Q3)
+    Q1 = df[columns].quantile(0.25)
+    Q3 = df[columns].quantile(0.75)
+
+    # Calculate the IQR (Interquartile Range)
+    IQR = Q3 - Q1
+
+    # Define the upper and lower bounds for outlier detection
+    lower_bound = Q1 - threshold * IQR
+    upper_bound = Q3 + threshold * IQR
+
+    # Remove rows containing outliers
+    filtered_df = df[~((df[columns] < lower_bound) | (df[columns] > upper_bound)).any(axis=1)]
+
+    return filtered_df
+
+def select_10_best_features(X,y,df):
+    target_column = 'prices.pricePerSquareFoot'
+    selector = SelectKBest(f_regression, k=10)  # Select top 10 features based on f_regression score
+    X_selected = selector.fit_transform(X, y)
+    selected_feature_indices = selector.get_support(indices=True)
+    selected_features = df.drop(target_column, axis=1).columns[selected_feature_indices]
+    return selected_features
+def y_outliers_removal(df):
+    Q1 = df['prices.pricePerSquareFoot'].quantile(0.25)
+    Q3 = df['prices.pricePerSquareFoot'].quantile(0.75)
+    threshold=1.3
+    # Calculate the IQR (Interquartile Range)
+    IQR = Q3 - Q1
+
+    # Define the upper and lower bounds for outlier detection
+    lower_bound = Q1 - threshold * IQR
+    upper_bound = Q3 + threshold * IQR
+    outliers_low=(df['prices.pricePerSquareFoot']<lower_bound)
+    outliers_high=(df['prices.pricePerSquareFoot']>upper_bound)
+    df['prices.pricePerSquareFoot'][outliers_low|outliers_high]
+    df=df[~(outliers_low|outliers_high)]
+    return df
+
 def train_model():
     df = pd.read_csv('compressed data.csv')
 
     df = df.drop_duplicates()
     df = drop_columns_with_high_null(df, threshold=0.75)
-    df.drop(["id", "city", "country", "county", "countyFIPS","mostRecentPriceDomain","prices.currency", "prices.isSale"], axis=1, inplace=True)
+    df.drop(["Unnamed: 0","id", "city", "country", "county", "countyFIPS","mostRecentPriceDomain","prices.currency", "prices.isSale"], axis=1, inplace=True)
 
     categorical_cols, numerical_cols = segregate_columns(df)
 
@@ -38,26 +78,47 @@ def train_model():
     for i in categorical_cols:
         df[i].fillna(df[i].mode()[0], inplace=True)
 
-    df = drop_columns_with_high_null(df, threshold=0.75)
 
     for col in categorical_cols:
         label_encoder = LabelEncoder()
         df[col] = label_encoder.fit_transform(df[col])
-
+        
+    df=y_outliers_removal(df)
     target_column = 'prices.pricePerSquareFoot'
     X = df.drop(target_column, axis=1)
     y = df[target_column]
+    
+    
+    #selector = SelectKBest(f_regression, k=10)  # Select top 10 features based on f_regression score
+    #X_selected = selector.fit_transform(X, y)
+    #selected_feature_indices = selector.get_support(indices=True)
 
+# Get the names of the selected features
+    #selected_features = df.drop(target_column, axis=1).columns[selected_feature_indices]
+    #print(selected_features)
+    #print(select_10_best_features(X,y))
+    #columns_to_check = selected_features
+    #df_without_outliers = remove_outliers_iqr(df, columns_to_check, threshold=1.5)
+    
+    #print("Top 10 Features:")
+    #for feature in selected_features:
+     #   print(feature)
+ 
+   # Assuming your DataFrame is named 'df'
+   # Replace 'columns_to_check' with the columns you want to check for outliers
+    
     scaler = StandardScaler()
-    df_scaled = scaler.fit_transform(df.drop(target_column, axis=1))  # Drop the target column before scaling
+    df_scaled = scaler.fit_transform(X)  # Drop the target column before scaling
 
     # Step 4: Feature Selection
+
     X = df_scaled
     y = df[target_column]
 
-    model = RandomForestRegressor()
-    model.fit(X, y)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+    model = RandomForestRegressor()
+    model.fit(X,y)
     # Save the trained model to a pickle file
     with open('trained_model.pkl', 'wb') as file:
         pickle.dump(model, file)
@@ -71,8 +132,8 @@ def predict_model(input_df: pd.DataFrame) -> pd.DataFrame:
     # New dataframe for prediction
     input_df_cleaned = input_df.drop_duplicates()
     input_df_copy = input_df_cleaned.copy()
-    input_df_cleaned = drop_columns_with_high_null(input_df_cleaned, threshold=0.75)
-    input_df_cleaned.drop(["id", "city", "country", "county", "countyFIPS","mostRecentPriceDomain","prices.currency", "prices.isSale"], axis=1, inplace=True)
+    input_df_cleaned.drop(['apiURLs', 'buildingName', 'congressionalDistrictHouse', 'deposits', 'languagesSpoken', 'leasingTerms', 'numPeople', 'numUnit', 'paymentTypes', 'people', 'petPolicy', 'prices.comment', 'prices.date', 'prices.dateValidStart', 'prices.dateValidEnd', 'prices.minStay', 'prices.period', 'reviews', 'rules', 'taxID', 'yearBuilt', 'taxID.1'],axis=1,inplace=True)
+    input_df_cleaned.drop(["Unnamed: 0.1","Unnamed: 0","id", "city", "country", "county", "countyFIPS","mostRecentPriceDomain","prices.currency", "prices.isSale"], axis=1, inplace=True)
 
     categorical_cols, numerical_cols = segregate_columns(input_df_cleaned)
 
@@ -80,8 +141,6 @@ def predict_model(input_df: pd.DataFrame) -> pd.DataFrame:
         input_df_cleaned[i].fillna(input_df_cleaned[i].mean(), inplace=True)
     for i in categorical_cols:
         input_df_cleaned[i].fillna(input_df_cleaned[i].mode()[0], inplace=True)
-
-    input_df_cleaned = drop_columns_with_high_null(input_df_cleaned, threshold=0.75)
 
     for col in categorical_cols:
         label_encoder = LabelEncoder()
